d19176cec587cb02383899efe4d0598cfecb9729
omakasebar <omakasebar@gmail.com>
2021-05-13 20:38:55 -0500

    savepoint

--- src/pages/Yield/masterchefv1/index.tsx
+++ src/pages/Yield/masterchefv1/index.tsx
@@ -10,6 +10,10 @@ import InputGroup from './InputGroup'
 import { SimpleDots as Dots } from 'kashi/components'
 import { Helmet } from 'react-helmet'
 
+import Badge from '../../../components/Badge'
+import _ from 'lodash'
+import { getTokenIcon } from '../../../kashi/functions'
+
 export const FixedHeightRow = styled(RowBetween)`
     height: 24px;
 `
@@ -18,6 +22,8 @@ export default function Yield(): JSX.Element {
     const query = useFarms()
     const farms = query?.farms
     const userFarms = query?.userFarms
+
+    const tvl = _.sumBy(farms, 'tvl')
 
     // Search Setup
     const options = { keys: ['symbol', 'name', 'pairAddress'], threshold: 0.4 }
@@ -35,15 +41,23 @@ export default function Yield(): JSX.Element {
                 <title>Yield | Sushi</title>
                 <meta name="description" content="Farm SUSHI by staking LP (Liquidity Provider) tokens" />
             </Helmet>
-            <div className="container max-w-2xl mx-auto">
+            <div className="container max-w-4xl mx-auto">
                 <Card
                     className="h-full bg-dark-900"
                     header={
                         <CardHeader className="flex justify-between items-center bg-dark-800">
                             <div className="flex w-full justify-between">
-                                <div className="hidden md:flex items-center">
+                                <div className="hidden md:block items-center">
                                     {/* <BackButton defaultRoute="/pool" /> */}
-                                    <div className="text-lg mr-2 whitespace-nowrap">Yield Instruments</div>
+                                    <div className="text-lg mr-2 whitespace-nowrap flex items-center">
+                                        <div className="mr-2">Yield Instruments</div>
+                                        <Badge color="blue">V1 Rewarder</Badge>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="text-sm text-gray-500 mr-2">
+                                            Total Deposits: {farms && farms.length > 0 && formattedNum(tvl, true)}
+                                        </div>
+                                    </div>
                                 </div>
                                 <Search search={search} term={term} />
                             </div>
@@ -54,15 +68,18 @@ export default function Yield(): JSX.Element {
                     {userFarms && userFarms.length > 0 && (
                         <>
                             <div className="pb-4">
-                                <div className="grid grid-cols-3 pb-4 px-4 text-sm  text-secondary">
+                                <div className="grid grid-cols-3 md:grid-cols-4 pb-4 px-4 text-sm  text-secondary">
                                     <div className="flex items-center">
                                         <div>Your Yields</div>
+                                    </div>
+                                    <div className="hidden md:block ml-4">
+                                        <div>Claim</div>
                                     </div>
                                     <div className="flex items-center justify-end">
                                         <div>Deposited</div>
                                     </div>
                                     <div className="flex items-center justify-end">
-                                        <div>Claim</div>
+                                        <div>APY</div>
                                     </div>
                                 </div>
                                 <div className="flex-col space-y-2">
@@ -74,7 +91,7 @@ export default function Yield(): JSX.Element {
                         </>
                     )}
                     {/* All Farms */}
-                    <div className="grid grid-cols-3 pb-4 px-4 text-sm  text-secondary">
+                    <div className="grid grid-cols-3 md:grid-cols-4 pb-4 px-4 text-sm  text-secondary">
                         <div
                             className="flex items-center cursor-pointer hover:text-secondary"
                             onClick={() => requestSort('symbol')}
@@ -84,6 +101,12 @@ export default function Yield(): JSX.Element {
                                 sortConfig.key === 'symbol' &&
                                 ((sortConfig.direction === 'ascending' && <ChevronUp size={12} />) ||
                                     (sortConfig.direction === 'descending' && <ChevronDown size={12} />))}
+                        </div>
+                        <div className="hidden md:block ml-4">
+                            <div className="flex items-center justify-start">
+                                <div className="pr-2">Pool Rewards</div>
+                                <Badge color="blue">2X</Badge>
+                            </div>
                         </div>
                         <div className="hover:text-secondary cursor-pointer" onClick={() => requestSort('tvl')}>
                             <div className="flex items-center justify-end">
@@ -96,7 +119,7 @@ export default function Yield(): JSX.Element {
                         </div>
                         <div className="hover:text-secondary cursor-pointer" onClick={() => requestSort('roiPerYear')}>
                             <div className="flex items-center justify-end">
-                                <div>APR</div>
+                                <div>APY (incl. Fees)</div>
                                 {sortConfig &&
                                     sortConfig.key === 'roiPerYear' &&
                                     ((sortConfig.direction === 'ascending' && <ChevronUp size={12} />) ||
@@ -134,7 +157,22 @@ const TokenBalance = ({ farm }: any) => {
             {farm.type === 'SLP' && (
                 <Paper className="bg-dark-800">
                     <div
-                        className="grid grid-cols-3 py-4 px-4 cursor-pointer select-none rounded text-sm"
+                        className="bg-dark-850 grid grid-cols-3 md:grid-cols-4 px-4 py-2  cursor-pointer select-none rounded rounded-b-none"
+                        onClick={() => setExpand(!expand)}
+                    >
+                        <div className="text-sm sm:text-base font-semibold">
+                            {farm && farm.liquidityPair.token0.symbol + '-' + farm.liquidityPair.token1.symbol}
+                        </div>
+                        <div className="hidden md:block text-sm sm:text-base ml-4 text-gray-500">{'SUSHI'}</div>
+                        <div className="text-gray-500 text-sm sm:text-base text-right">
+                            {formattedNum(farm.tvl, true)}
+                        </div>
+                        <div className="font-semibold text-sm sm:text-base text-right">
+                            {farm.roiPerYear > 100 ? '10000%+' : formattedPercent(farm.roiPerYear * 100)}
+                        </div>
+                    </div>
+                    <div
+                        className="grid grid-cols-3 md:grid-cols-12 py-4 px-4 cursor-pointer select-none rounded text-sm"
                         onClick={() => setExpand(!expand)}
                     >
                         <div className="flex items-center">
@@ -142,25 +180,48 @@ const TokenBalance = ({ farm }: any) => {
                                 <DoubleLogo
                                     a0={farm.liquidityPair.token0.id}
                                     a1={farm.liquidityPair.token1.id}
-                                    size={32}
+                                    size={40}
                                     margin={true}
                                 />
                             </div>
-                            <div className="hidden sm:block">
+                            {/* <div className="hidden sm:block">
                                 {farm && farm.liquidityPair.token0.symbol + '-' + farm.liquidityPair.token1.symbol}
+                            </div> */}
+                        </div>
+                        <div className="md:col-span-4 hidden md:flex flex-row space-x-2 justify-start items-center ml-4">
+                            <div>
+                                <img
+                                    src={getTokenIcon('0x6B3595068778DD592e39A122f4f5a5cF09C90fE2', 1)}
+                                    className="block w-10 h-10 rounded-full"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="flex flex-col pl-2 space-y-1">
+                                <div className="text-gray-500 text-xs">
+                                    {formattedNum(farm.sushiRewardPerDay)} SUSHI / day
+                                </div>
+                                {/* <div className="text-gray-500 text-xs">
+                                    {formattedNum(farm.secondaryRewardPerDay)} MATIC / day
+                                </div> */}
                             </div>
                         </div>
-                        <div className="flex justify-end items-center">
+                        <div className="md:col-span-2 flex justify-end items-center">
                             <div>
-                                <div className="text-right">{formattedNum(farm.tvl, true)} </div>
-                                <div className="text-secondary text-right">
+                                {/* <div className="text-right">{formattedNum(farm.tvl, true)} </div> */}
+                                <div className="text-gray-500 text-right font-semibold text-sm sm:text-sm">
                                     {formattedNum(farm.slpBalance / 1e18, false)} SLP
                                 </div>
+                                <div className="text-gray-500 text-right text-xs">Market Staked</div>
                             </div>
                         </div>
-                        <div className="flex justify-end items-center">
-                            <div className="text-right font-semibold text-xl">
-                                {farm.roiPerYear > 10000 ? '10000%+' : formattedPercent(farm.roiPerYear * 100)}
+                        <div className="md:col-span-3 flex justify-end items-center">
+                            <div>
+                                <div className="text-gray-500 text-right font-semibold text-base sm:text-lg">
+                                    {farm.roiPerYear > 100 ? '10000%+' : formattedPercent(farm.roiPerYear * 100)}
+                                    {/* {formattedPercent(farm.roiPerMonth * 100)}{' '} */}
+                                </div>
+                                <div className="text-gray-500 text-right text-xs">annualized</div>
+                                {/* <div className="text-gray-500 text-right text-xs">per month</div> */}
                             </div>
                         </div>
                     </div>
