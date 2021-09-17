64896c0ade239b25eab9f1974f754bda59611e36
Kasumi <kasumi.null@yandex.com>
2021-09-17 15:38:07 -0500

    feat: add zhesto list

--- src/config/token-lists.ts
+++ src/config/token-lists.ts
@@ -6,10 +6,13 @@ const BA_LIST = 'https://raw.githubusercontent.com/The-Blockchain-Association/se
  */
 export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 
+const ZHESTO_LIST = 'https://raw.githubusercontent.com/zh/sep20tokens/main/smartbch.tokenlist.json'
+
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
+  ZHESTO_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [ZHESTO_LIST]
