31ad3b50f6100a984a7a29eb2d8fc84f181c83af
Kasumi <kasumi.null@yandex.com>
2021-09-20 01:38:56 -0500

    chore: set smartbch mainnet rpc

--- src/config/rpc.ts
+++ src/config/rpc.ts
@@ -1,8 +1,7 @@
 import { ChainId } from '@mistswapdex/sdk'
 
 const rpc = {
-  // [ChainId.SMARTBCH]: 'https://smartbch.fountainhead.cash/mainnet',
-  [ChainId.SMARTBCH]: 'http://127.0.0.1:8545',
+  [ChainId.SMARTBCH]: 'https://smartbch.fountainhead.cash/mainnet',
   [ChainId.SMARTBCH_AMBER]: 'http://35.220.203.194:8545',
 }
 
