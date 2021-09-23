c53064726ae55e52fee8d9e2d7311c05a712e23d
Kasumi <kasumi.null@yandex.com>
2021-09-23 04:15:52 -0500

    chore: change testnet rpc

--- src/config/rpc.ts
+++ src/config/rpc.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@mistswapdex/sdk'
 
 const rpc = {
   [ChainId.SMARTBCH]: 'https://smartbch.fountainhead.cash/mainnet',
-  [ChainId.SMARTBCH_AMBER]: 'http://35.220.203.194:8545',
+  [ChainId.SMARTBCH_AMBER]: 'https://moeing.tech:9545',
 }
 
 export default rpc
