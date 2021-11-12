c88db8e712bdca1b9e717282678513baba851865
Kasumi <kasumi.null@yandex.com>
2021-11-12 17:51:15 -0600

    feat: add past farms selector

--- src/features/onsen/FarmMenu.tsx
+++ src/features/onsen/FarmMenu.tsx
@@ -43,6 +43,15 @@ const Menu = ({ positionsLength }) => {
         </a>
       </NavLink>
 
+      <NavLink
+        href="/farm?filter=past"
+        activeClassName="font-bold bg-transparent border rounded text-high-emphesis border-transparent border-gradient-r-blue-pink-dark-900"
+      >
+        <a className="flex items-center justify-between px-2 py-3 md:px-4 md:py-6 text-base font-bold border border-transparent rounded cursor-pointer bg-dark-900 hover:bg-dark-800">
+          {i18n._(t`Past Farms`)}
+        </a>
+      </NavLink>
+
       {/*chainId === ChainId.MAINNET && (
         <>
           <NavLink
